b54125c8aebc27b0a16e0c24912d4051f0fe4123
Negarnbkhti42 <negar.nobakhti01@gmail.com>
2026-04-02 12:33:55 +0200

    update to new mp configs

--- HandTracking.py
+++ HandTracking.py
@@ -1,18 +1,28 @@
 import cv2
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 import time
 
 cap = cv2.VideoCapture(0)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-while True:
-    success, img = cap.read()
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path="../mp-models/hand_landmarker.task"),
+    running_mode=VisionRunningMode.VIDEO,
+)
 
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+with HandLandmarker.create_from_options(options) as landmarker:
+    while True:
+        success, img = cap.read()
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
